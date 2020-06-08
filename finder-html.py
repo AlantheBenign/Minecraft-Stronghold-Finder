324ebeaeca45c69fab66d0ce48ee791a225308be
Alan Sousa <66627307+AlantheBenign@users.noreply.github.com>
2020-06-08 14:53:29 -0300

    Add files via upload

---
+++ finder-html.py
@@ -0,0 +1,7 @@
+from browser import document
+from browser.template import template
+
+def nrm(event,element):
+	element.data.f3c0 = document['throw1'].value
+
+Template('throw1',[nmr]).render(f3c0="ixi")
