daeea5f12f56291f7abf2275d18a0d71b139f4c0
Alan Sousa <66627307+AlantheBenign@users.noreply.github.com>
2020-06-08 15:06:52 -0300

    Update finder-html.py

--- finder-html.py
+++ finder-html.py
@@ -4,4 +4,4 @@ from browser.template import template
 def nrm(event,element):
 	element.data.f3c0 = document['throw1'].value
 
-Template('throw1',[nmr]).render(f3c0="ixi")
+Template('space1',[nmr]).render(f3c0="")
