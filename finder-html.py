b8917d3a13d741557f03f9ef8512ce0418f0d562
Alan Sousa <66627307+AlantheBenign@users.noreply.github.com>
2020-06-08 21:36:07 -0300

    Delete finder-html.py

--- finder-html.py
+++
@@ -1,7 +0,0 @@
-from browser import document
-from browser.template import Template
-
-def nrm(event,element):
-	element.data.myname = document['write'].value
-
-Template('name',[nmr]).render(myname="")
